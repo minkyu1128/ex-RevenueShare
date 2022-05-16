61405ab4745884f39baf2305b3b54709da6c4414
minkyu1128 <minkyu1128@gmail.com>
2022-05-16 19:56:34 +0900

    테스트플랜 작성

--- src/test/java/com/example/revenueshare/RevenueShareApplicationTests.java
+++ src/test/java/com/example/revenueshare/RevenueShareApplicationTests.java
@@ -1,9 +1,7 @@
 package com.example.revenueshare;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -13,6 +11,15 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @ExtendWith(SpringExtension.class)
 @ActiveProfiles({"test"})
 class RevenueShareApplicationTests {
+
+
+    /* ================================================
+    * [테스트 Plan]
+    *  -. Import 또는 ApplicationRunner를 이용한 테스트 Data Setup
+    *  -. CRUD 테스트 케이스 작성
+    *  -. @Display에 테스트 케이스 목적 작성
+    *  -. then 절 Assert 지향
+    ================================================ */
 
 //    @Test
 //    void contextLoads() {
